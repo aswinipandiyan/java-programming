ad6a176b6d026519b12ffacf786632dfbe19a7c9
aswinipandiyan <43059525+aswinipandiyan@users.noreply.github.com>
2018-09-07 12:21:55 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args){
+   int a,b,c,d,e,f;
+ a=1;
+ b=2;
+ c=a+b;
+ d=a-b;
+ e=a*b;
+ f=a/b;
+ System.out.println("add the value"+c);
+ System.out.println("sub the value"+d);
+ system.out.println("mul the value"+e);
+ system.out.println("duv the value"+f);
+  }
+}
