ad13c3680fc2faba2b4d3becd47d0b23aea3e40c
aswinipandiyan <43059525+aswinipandiyan@users.noreply.github.com>
2018-09-07 12:25:19 +0530

    Add files via upload

---
+++ double.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args){
+   int a;
+   double b,c,d,e,f;
+ a=1;
+ b=2;
+ c=a+b;
+ d=a-b;
+ e=a*b;
+ f=a/b;
+ System.out.println("add the value"+c);
+ System.out.println("sub the value"+d);
+ System.out.println("mul the value"+e);
+ System.out.println("div the value"+f);
+  }
+}
