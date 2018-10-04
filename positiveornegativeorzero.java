5f58b9131124bdfb8d8c23ef9aaad96111d4f679
aswinipandiyan <43059525+aswinipandiyan@users.noreply.github.com>
2018-10-04 12:38:37 +0530

    Add files via upload

---
+++ positiveornegativeorzero.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args){
+      int number=3;
+          if(number > 0)
+          {
+         System.out.println(" Positive ");
+        }
+        else if(number < 0)
+        {
+          System.out.println(" Negative ");
+        }
+        else if (number == 0)
+        {
+            System.out.println(" zero ");
+        }
+        else
+        {
+            System.out.println(" Positive or Negative or zero ");
+        }
+    }
+}  
+    
