d1643c14348b96c3237c2e65197df113cd80bee2
aswinipandiyan <43059525+aswinipandiyan@users.noreply.github.com>
2018-10-05 11:57:23 +0530

    Add files via upload

---
+++ oddoreven.java
@@ -0,0 +1,13 @@
+ class Main {
+  public static void main(String[] args) {
+    int n=22212;
+       if (n % 2 == 0) 
+       {
+         System.out.println(" even ");
+       }
+      else
+      {
+         System.out.println(" odd ");
+      }  
+   }
+}
