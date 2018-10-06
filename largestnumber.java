c6ab4084af408819ed52c30cfb12cca0d2aa95b7
aswinipandiyan <43059525+aswinipandiyan@users.noreply.github.com>
2018-10-06 12:34:11 +0530

    Add files via upload

---
+++ largestnumber.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    double n1 = 1, n2 = 1, n3 = -4;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println( n1+ "  ");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println( n2+ " ");
+
+        else
+            System.out.println( n3+ " ");
+    }
+}
